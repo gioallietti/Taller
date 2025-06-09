12ce2ab8a506a6b5e72c0370757d9d2036d1a42e
Giovanni <giovannicolonia@gmail.com>
2025-06-09 18:06:24 -0300

    ABM estado, prioridad y persona

--- src/main/java/com/example/Taller/Service/PresupuestoServiceImpl.java
+++ src/main/java/com/example/Taller/Service/PresupuestoServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class PresupuestoServiceImpl extends PresupuestoService{
+public class PresupuestoServiceImpl implements PresupuestoService{
     @Autowired
     private PresupuestoRepository presupuestoRepository;
 
