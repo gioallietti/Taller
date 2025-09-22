23caf04b6b274403f1de4d8dfc7510c0ea50a63c
gioallietti <giovannicolonia@gmail.com>
2025-09-22 15:12:58 -0300

    Filtro de ganancias por fechas

--- src/main/java/com/example/Taller/Repository/PresupuestoRepository.java
+++ src/main/java/com/example/Taller/Repository/PresupuestoRepository.java
@@ -1,15 +1,19 @@
 package com.example.Taller.Repository;
 
 import com.example.Taller.Entity.PresupuestoEntity;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface PresupuestoRepository extends JpaRepository<PresupuestoEntity, Integer> {
 
     PresupuestoEntity findByIngreso_Id(Integer ingresoId);
+
+    List<PresupuestoEntity> findByFechaBetween(LocalDate fechaInicio, LocalDate fechaFin);
 
     @Query(value = "SELECT year(fecha) as año, sum(total_iva), sum(mano_de_obra), sum(costo_repuesto) \n" +
             "FROM taller4.presupuestos \n" +
